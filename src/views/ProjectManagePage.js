5e9cf5e9068acfb113bbf42ad0472a164d2c1e85
Shih Yun Chen <b05705019@ntu.edu.tw>
2021-08-19 18:03:10 +0800

    eslint formating

--- src/views/ProjectManagePage.js
+++ src/views/ProjectManagePage.js
@@ -1,21 +1,21 @@
-import './ParagraphCards.css'
-import './ProjectManagePage.css'
-import './Labeling.css'
-import 'react-responsive-modal/styles.css';
-import { Modal } from 'react-responsive-modal';
-import EditProjectPage from './EditProjectPage' // temp
-import AddProjectPage from './AddProjectPage' // temp
-import Header from '../components/Header'
-import { BASEURL } from "../config"
-import { useState, useEffect } from "react"
-import { useSelector} from 'react-redux'
-import axios from 'axios'
+import "./ParagraphCards.css";
+import "./ProjectManagePage.css";
+import "./Labeling.css";
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
+import EditProjectPage from "./EditProjectPage"; // temp
+import AddProjectPage from "./AddProjectPage"; // temp
+import Header from "../components/Header";
+import { BASEURL } from "../config";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
 
 function ProjectManagePage() {
   const [projects, setProjects] = useState([]);
   const [focusProject, setFocusProject] = useState();
 
-  const profileObj = useSelector(state => state.accountReducer.profileObj);
+  const profileObj = useSelector((state) => state.accountReducer.profileObj);
   /** open edit modal */
   const [open, setOpen] = useState(false);
   /** open add project modal */
@@ -29,54 +29,68 @@ function ProjectManagePage() {
       const arg = {
         userId: profileObj.googleId,
         statusCode: "1",
-      }
-      const res = await axios.post(`${BASEURL}/projects`, arg)
+      };
+      const res = await axios.post(`${BASEURL}/projects`, arg);
       setProjects(res.data);
-      console.log("projects", res.data)
+      console.log("projects", res.data);
     };
-    getProject()
-  }, [profileObj.googleId, openAdd])
+    getProject();
+  }, [profileObj.googleId, openAdd]);
 
   const editProject = (project) => {
     setFocusProject(project);
     setOpen(true);
-    return
-  }
+    return;
+  };
 
   const addProject = (project) => {
     setOpenAdd(true);
-    return
-  }
+    return;
+  };
 
   return (
     <div>
-      <Header isManagePage={true}/>
+      <Header isManagePage={true} />
       <div id="project" className="justify-center">
         <div className="working-area-container overflow-scroll project-working-area">
           <div className="justify-center">
             <h2>我管理的專案</h2>
           </div>
           <div className="center-center flex-wrap">
-            {projects.sort(function (a, b){return a.projectId - b.projectId;}).map((project, idx) => (
-              <div key={idx} className="center-center list-item" onClick={() => { editProject(project) }}>
-                <div key={idx}>
-                    {project.projectName}
+            {projects
+              .sort(function (a, b) {
+                return a.projectId - b.projectId;
+              })
+              .map((project, idx) => (
+                <div
+                  key={idx}
+                  className="center-center list-item"
+                  onClick={() => {
+                    editProject(project);
+                  }}
+                >
+                  <div key={idx}>{project.projectName}</div>
                 </div>
-              </div>
-            ))}
-            <div className="function-button" onClick={addProject}>新增專案</div>
+              ))}
+            <div className="function-button" onClick={addProject}>
+              新增專案
+            </div>
           </div>
         </div>
         <Modal open={open} onClose={onCloseModal} center>
           {/* <EditProjectPage project={focusProject} onCloseCallback={onCloseModal}/> */}
-          <AddProjectPage project={focusProject} isEdit={true} onCloseCallback={onCloseAddModal}/>
+          <AddProjectPage
+            project={focusProject}
+            isEdit={true}
+            onCloseCallback={onCloseAddModal}
+          />
         </Modal>
         <Modal open={openAdd} onClose={onCloseAddModal} center>
-          <AddProjectPage onCloseCallback={onCloseAddModal}/>
+          <AddProjectPage onCloseCallback={onCloseAddModal} />
         </Modal>
       </div>
     </div>
-  )
+  );
 }
 
 export default ProjectManagePage;
--- src/views/ValidationPage.js
+++ src/views/ValidationPage.js
@@ -1,26 +1,26 @@
-import './ValidationPage.css'
-import './Labeling.css'
-import { BASEURL } from "../config"
-import { useState, useEffect } from "react"
-import { useSelector} from 'react-redux'
-import axios from 'axios'
+import "./ValidationPage.css";
+import "./Labeling.css";
+import { BASEURL } from "../config";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
 
 function ValidationPage() {
-  const [task, setTask] = useState("")
-  const profileObj = useSelector(state => state.accountReducer.profileObj)
+  const [task, setTask] = useState("");
+  const profileObj = useSelector((state) => state.accountReducer.profileObj);
 
   const getTask = async () => {
     const arg = {
       userId: profileObj.googleId,
-      taskType: "MRCValidation"
-    }
-    const res = await axios.post(`${BASEURL}/getValidation`, arg)
+      taskType: "MRCValidation",
+    };
+    const res = await axios.post(`${BASEURL}/getValidation`, arg);
     setTask(res.data);
-  }
+  };
 
   useEffect(() => {
     getTask();
-  }, [profileObj.googleId])
+  }, [profileObj.googleId]);
 
   return (
     <div id="validation" className="justify-center">
@@ -33,18 +33,19 @@ function ValidationPage() {
         </div>
         <div className="justify-start mb-30 body-padding">
           <div className="nowrap mr-10">問題：</div>
-            {task.question}
+          {task.question}
         </div>
         <div className="justify-start body-padding">
           <div className="nowrap mr-10">答案：</div>
           <textarea
             className="working-textarea"
-            placeholder="請透過滑鼠反白方式選擇文章中的答案" />
+            placeholder="請透過滑鼠反白方式選擇文章中的答案"
+          />
         </div>
         <div className="justify-center"></div>
       </div>
     </div>
-  )
+  );
 }
 
 export default ValidationPage;
