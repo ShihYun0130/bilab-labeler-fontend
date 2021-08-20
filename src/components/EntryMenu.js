3057d7b03bc5a0cc70978b14daeda110be8d1725
Shih Yun Chen <b05705019@ntu.edu.tw>
2021-08-21 00:55:17 +0800

    prettier formatting

--- src/components/EntryMenu.js
+++ src/components/EntryMenu.js
@@ -1,171 +1,182 @@
-import { useEffect, useState, useRef } from "react";
-import Button from "@material-ui/core/Button";
-import ClickAwayListener from "@material-ui/core/ClickAwayListener";
-import Grow from "@material-ui/core/Grow";
-import Paper from "@material-ui/core/Paper";
-import Popper from "@material-ui/core/Popper";
-import MenuItem from "@material-ui/core/MenuItem";
-import MenuList from "@material-ui/core/MenuList";
-import { makeStyles } from "@material-ui/core/styles";
-import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import axios from "axios";
-import { useDispatch } from "react-redux";
+import { useEffect, useState, useRef } from "react"
+import Button from "@material-ui/core/Button"
+import ClickAwayListener from "@material-ui/core/ClickAwayListener"
+import Grow from "@material-ui/core/Grow"
+import Paper from "@material-ui/core/Paper"
+import Popper from "@material-ui/core/Popper"
+import MenuItem from "@material-ui/core/MenuItem"
+import MenuList from "@material-ui/core/MenuList"
+import { makeStyles } from "@material-ui/core/styles"
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown"
+import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
+import axios from "axios"
+import { useDispatch } from "react-redux"
 
-import { MRC_BASEURL } from "../config";
+import { MRC_BASEURL } from "../config"
 
 const useStyles = makeStyles(() => ({
-  root: {
-    display: "flex",
-  },
-  button: {
-    fontFamily: "Poppins, sans-serif",
-    fontSize: "25px",
-    color: "#6184C6",
-  },
-  paper: {
-    borderRadius: "10px",
-    color: "#6184C6",
-    width: "150%",
-  },
-  link: {
-    textDecoration: "none",
-    color: "#6184C6",
-  },
-}));
+    root: {
+        display: "flex",
+    },
+    button: {
+        fontFamily: "Poppins, sans-serif",
+        fontSize: "25px",
+        color: "#6184C6",
+    },
+    paper: {
+        borderRadius: "10px",
+        color: "#6184C6",
+        width: "150%",
+    },
+    link: {
+        textDecoration: "none",
+        color: "#6184C6",
+    },
+}))
 
 export default function EntryMenu() {
-  const focusProject = useSelector(
-    (state) => state.projectReducer.focusProject
-  );
-  const classes = useStyles();
-  const [open, setOpen] = useState(false);
-  const anchorRef = useRef(null);
-  const [taskTypeTitle, setTaskTypeTitle] = useState("");
+    const focusProject = useSelector(
+        (state) => state.projectReducer.focusProject
+    )
+    const classes = useStyles()
+    const [open, setOpen] = useState(false)
+    const anchorRef = useRef(null)
+    const [taskTypeTitle, setTaskTypeTitle] = useState("")
 
-  // query available tasks
-  const profileObj = useSelector((state) => state.accountReducer.profileObj);
-  const [projects, setProjects] = useState();
+    // query available tasks
+    const profileObj = useSelector((state) => state.accountReducer.profileObj)
+    const userId = useSelector((state) => state.accountReducer.userId)
+    const [projects, setProjects] = useState()
 
-  // change redux status and write to localStorage
-  const dispatch = useDispatch();
-  const dispatchProject = (item) => {
-    dispatch({
-      type: "SETPROJECT",
-      payload: { focusProject: item },
-    });
-  };
-
-  useEffect(() => {
-    const getProject = async () => {
-      const arg = {
-        userId: profileObj.googleId,
-        statusCode: "0",
-      };
-      const res = await axios.get(`${MRC_BASEURL}/projects`, arg);
-      setProjects(res.data);
-      if (res.data.length) {
-        setTaskTypeTitle(focusProject.type);
-      }
-    };
-    getProject();
-  }, [profileObj.googleId]);
-
-  const handleToggle = () => {
-    setOpen((prevOpen) => !prevOpen);
-  };
-
-  const handleClose = (event) => {
-    if (anchorRef.current && anchorRef.current.contains(event.target)) {
-      return;
+    // change redux status and write to localStorage
+    const dispatch = useDispatch()
+    const dispatchProject = (item) => {
+        dispatch({
+            type: "SETPROJECT",
+            payload: { focusProject: item },
+        })
     }
 
-    setOpen(false);
-  };
+    useEffect(() => {
+        const getProject = async () => {
+            const arg = {
+                userId: userId,
+                statusCode: "0",
+            }
+            const res = await axios.get(`${MRC_BASEURL}/projects`)
+            const projectsData = res.data.map((data) => ({
+                projectId: data._id,
+                projectName: data.name,
+                projectType: data.type,
+                labelInfo: data.rule,
+            }))
+            console.log("projects", projectsData)
+            setProjects(projectsData)
+            if (res.data.length) {
+                setTaskTypeTitle(focusProject.projectName)
+            }
+        }
+        getProject()
+    }, [profileObj.googleId])
 
-  const handleMenuItemClick = (event, index) => {
-    // setSelectedIndex(index);
-    // setAnchorEl(null);
-    setTaskTypeTitle(projects[index].type);
-    dispatchProject(projects[index]);
-    handleClose(event);
-  };
-
-  function handleListKeyDown(event) {
-    if (event.key === "Tab") {
-      event.preventDefault();
-      setOpen(false);
+    const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen)
     }
-  }
 
-  // return focus to the button when we transitioned from !open -> open
-  const prevOpen = useRef(open);
-  useEffect(() => {
-    if (prevOpen.current === true && open === false) {
-      anchorRef.current.focus();
+    const handleClose = (event) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+            return
+        }
+
+        setOpen(false)
     }
 
-    prevOpen.current = open;
-  }, [open]);
+    const handleMenuItemClick = (event, index) => {
+        // setSelectedIndex(index);
+        // setAnchorEl(null);
+        setTaskTypeTitle(projects[index].projectName)
+        dispatchProject(projects[index])
+        handleClose(event)
+    }
 
-  return (
-    <div className={classes.root}>
-      <div>
-        <Button
-          ref={anchorRef}
-          aria-controls={open ? "menu-list-grow" : undefined}
-          aria-haspopup="true"
-          onClick={handleToggle}
-          className={classes.button}
-        >
-          {taskTypeTitle}
-          <ArrowDropDownIcon />
-        </Button>
-        <Popper
-          open={open}
-          placement="bottom-start"
-          anchorEl={anchorRef.current}
-          role={undefined}
-          transition
-          disablePortal
-        >
-          {({ TransitionProps }) => (
-            <Grow {...TransitionProps}>
-              <Paper className={classes.paper}>
-                <ClickAwayListener onClickAway={handleClose}>
-                  <MenuList
-                    autoFocusItem={open}
-                    id="menu-list-grow"
-                    onKeyDown={handleListKeyDown}
-                  >
-                    {projects
-                      .sort(function (a, b) {
-                        return a._id - b._id;
-                      })
-                      .map((project, index) => (
-                        <Link
-                          key={index}
-                          className={classes.link}
-                          to={`/${project.type}/Label/${project._id}`}
-                        >
-                          <MenuItem
-                            key={index}
-                            onClick={(event) =>
-                              handleMenuItemClick(event, index)
-                            }
-                          >
-                            {project.name}
-                          </MenuItem>
-                        </Link>
-                      ))}
-                  </MenuList>
-                </ClickAwayListener>
-              </Paper>
-            </Grow>
-          )}
-        </Popper>
-      </div>
-    </div>
-  );
+    function handleListKeyDown(event) {
+        if (event.key === "Tab") {
+            event.preventDefault()
+            setOpen(false)
+        }
+    }
+
+    // return focus to the button when we transitioned from !open -> open
+    const prevOpen = useRef(open)
+    useEffect(() => {
+        if (prevOpen.current === true && open === false) {
+            anchorRef.current.focus()
+        }
+
+        prevOpen.current = open
+    }, [open])
+
+    return (
+        <div className={classes.root}>
+            <div>
+                <Button
+                    ref={anchorRef}
+                    aria-controls={open ? "menu-list-grow" : undefined}
+                    aria-haspopup="true"
+                    onClick={handleToggle}
+                    className={classes.button}
+                >
+                    {taskTypeTitle}
+                    <ArrowDropDownIcon />
+                </Button>
+                <Popper
+                    open={open}
+                    placement="bottom-start"
+                    anchorEl={anchorRef.current}
+                    role={undefined}
+                    transition
+                    disablePortal
+                >
+                    {({ TransitionProps }) => (
+                        <Grow {...TransitionProps}>
+                            <Paper className={classes.paper}>
+                                <ClickAwayListener onClickAway={handleClose}>
+                                    <MenuList
+                                        autoFocusItem={open}
+                                        id="menu-list-grow"
+                                        onKeyDown={handleListKeyDown}
+                                    >
+                                        {projects
+                                            .sort(function (a, b) {
+                                                return a.projectId - b.projectId
+                                            })
+                                            .map((project, index) => (
+                                                <Link
+                                                    key={index}
+                                                    className={classes.link}
+                                                    to={`/${project.projectType}/Label/${project.projectId}`}
+                                                >
+                                                    <MenuItem
+                                                        key={index}
+                                                        onClick={(event) =>
+                                                            handleMenuItemClick(
+                                                                event,
+                                                                index
+                                                            )
+                                                        }
+                                                    >
+                                                        {project.projectName}
+                                                    </MenuItem>
+                                                </Link>
+                                            ))}
+                                    </MenuList>
+                                </ClickAwayListener>
+                            </Paper>
+                        </Grow>
+                    )}
+                </Popper>
+            </div>
+        </div>
+    )
 }
--- src/views/ValidationPage.js
+++ src/views/ValidationPage.js
@@ -1,48 +1,47 @@
-import "./ValidationPage.css";
-import "./Labeling.css";
-import { MRC_BASEURL } from "../config";
-import { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-import axios from "axios";
+import "./ValidationPage.css"
+import "./Labeling.css"
+import { MRC_BASEURL } from "../config"
+import { useState, useEffect } from "react"
+import { useSelector } from "react-redux"
+import axios from "axios"
 
 function ValidationPage() {
-  const [task, setTask] = useState("");
-  const userId = useSelector((state) => state.accountReducer.userId);
+    const [task, setTask] = useState()
+    const userId = useSelector((state) => state.accountReducer.userId)
 
-  const getTask = async () => {
-    const arg = {
-      userId: userId,
-    };
-    const res = await axios.get(`${MRC_BASEURL}/validation`, arg);
-    setTask(res.data);
-  };
-
-  useEffect(() => {
-    getTask();
-  }, [userId]);
-
-  return (
-    <div id="validation" className="justify-center">
-      <div className="working-area-container overflow-scroll validation-working-area">
-        <div className="working-article-title body-padding">{task.title}</div>
-        <div className="working-article-content body-padding">
-          {task.taskId.content}
+    useEffect(() => {
+        const getTask = async () => {
+            const res = await axios.get(`${MRC_BASEURL}/validation`, {
+                params: { userId: userId },
+            })
+            setTask(res.data)
+        }
+        getTask()
+    }, [userId])
+    return (
+        <div id="validation" className="justify-center">
+            <div className="working-area-container overflow-scroll validation-working-area">
+                <div className="working-article-title body-padding">
+                    {task.title}
+                </div>
+                <div className="working-article-content body-padding">
+                    {task.taskId.content}
+                </div>
+                <div className="justify-start mb-30 body-padding">
+                    <div className="nowrap mr-10">問題：</div>
+                    {task.question}
+                </div>
+                <div className="justify-start body-padding">
+                    <div className="nowrap mr-10">答案：</div>
+                    <textarea
+                        className="working-textarea"
+                        placeholder="請透過滑鼠反白方式選擇文章中的答案"
+                    />
+                </div>
+                <div className="justify-center"></div>
+            </div>
         </div>
-        <div className="justify-start mb-30 body-padding">
-          <div className="nowrap mr-10">問題：</div>
-          {task.question}
-        </div>
-        <div className="justify-start body-padding">
-          <div className="nowrap mr-10">答案：</div>
-          <textarea
-            className="working-textarea"
-            placeholder="請透過滑鼠反白方式選擇文章中的答案"
-          />
-        </div>
-        <div className="justify-center"></div>
-      </div>
-    </div>
-  );
+    )
 }
 
-export default ValidationPage;
+export default ValidationPage
