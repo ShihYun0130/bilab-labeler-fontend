105f47c2d30792c26958ab1e125b8578a01ddd10
Shih Yun Chen <b05705019@ntu.edu.tw>
2021-08-21 22:48:36 +0800

    finish decision page in validation page

--- src/views/ValidationPage.js
+++ src/views/ValidationPage.js
@@ -5,20 +5,45 @@ import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 import Loader from 'react-loader-spinner';
+import {
+  FormLabel,
+  FormControl,
+  RadioGroup,
+  Radio,
+  FormControlLabel,
+} from '@material-ui/core';
+import { CompassCalibrationOutlined } from '@material-ui/icons';
 
 function ValidationPage() {
-  const [task, setTask] = useState();
+  const [validation, setValidation] = useState();
+  const [decision, setDecision] = useState();
   const userId = useSelector((state) => state.accountReducer.userId);
   const [isFixedAnswer, setIsFixedAnswer] = useState(false);
-  const [answer, setAnswer] = useState('');
+  const [validationAnswer, setValidationAnswer] = useState('');
   const [startIndex, setStartIndex] = useState(0);
+  const [decisionResult, setDecisionResult] = useState('answer');
+
+  const handleDecisionChange = (event) => {
+    console.log(event.target.value);
+    setDecisionResult(event.target.value);
+  };
 
   useEffect(() => {
+    // if there is decision data, render decision question,
+    // if not, render validation question
+    // if neither, render keep labeling message
     const getTask = async () => {
-      const res = await axios.get(`${MRC_BASEURL}/validation`, {
+      const res = await axios.get(`${MRC_BASEURL}/decision`, {
         params: { userId: userId },
       });
-      setTask(res.data);
+      console.log('decision', res);
+      setDecision(res.data);
+      if (!res) {
+        const vali = await axios.get(`${MRC_BASEURL}/validation`, {
+          params: { userId: userId },
+        });
+        setValidation(vali.data);
+      }
     };
     getTask();
   }, [userId]);
@@ -29,7 +54,7 @@ function ValidationPage() {
     }
     event.stopPropagation();
     var selObj = window.getSelection();
-    setAnswer(selObj.toString());
+    setValidationAnswer(selObj.toString());
     var selRange = selObj.getRangeAt(0);
     setStartIndex(selRange.startOffset);
     return;
@@ -38,8 +63,8 @@ function ValidationPage() {
   const saveValidation = async () => {
     const validationData = {
       userId: userId,
-      answerId: task._id,
-      validationAnswer: answer,
+      answerId: validation._id,
+      validationAnswer: validationAnswer,
       validationStartIdx: startIndex,
     };
     const res = await axios.post(`${MRC_BASEURL}/validation`, validationData);
@@ -48,49 +73,117 @@ function ValidationPage() {
     return;
   };
 
-  if (!task) {
-    return (
-      <Loader
-        className="center"
-        type="RevolvingDot"
-        color="#4D87EB"
-        height={100}
-        width={100}
-        timeout={3000} //3 secs
-      />
-    );
-  }
+  const saveDecision = async () => {
+    const decisionData = {
+      userId: userId,
+      answerId: decision.answerId._id,
+      validationId: decision.validationId._id,
+      decisionId: decision._id,
+      decisionResult: decisionResult,
+    };
+    console.log('decision result', decisionData);
+    // const res = await axios.post(`${MRC_BASEURL}/decision`, decisionData);
+    // console.log('decision result', res);
+    // window.location.reload();
+    return;
+  };
+
+  // if (!validation && !decision) {
+  //   return (
+  //     <Loader
+  //       className="center"
+  //       type="RevolvingDot"
+  //       color="#4D87EB"
+  //       height={100}
+  //       width={100}
+  //       timeout={3000} //3 secs
+  //     />
+  //   );
+  // }
   return (
     <div id="validation" className="justify-center">
-      <div className="working-area-container overflow-scroll validation-working-area">
-        <div className="working-article-title body-padding">{task.title}</div>
-        <div
-          className="working-article-content body-padding"
-          onMouseUp={mouseUpHandler}
-        >
-          {task ? task.taskId.content : ''}
-        </div>
-        <div className="justify-start mb-30 body-padding">
-          <div className="nowrap mr-10">問題：</div>
-          {task.question}
-        </div>
-        <div className="justify-start body-padding">
-          <div className="nowrap mr-10">答案：</div>
-          <textarea
-            className="working-textarea"
-            value={answer}
-            onChange={() => {
-              return;
-            }}
-            placeholder="請透過滑鼠反白方式選擇文章中的答案"
-          />
-        </div>
-        <div className="justify-center">
-          <div onClick={() => saveValidation()}>
-            <div className="function-button">完成</div>
+      {!decision && !validation ? (
+        <div>沒有任何待驗證的題目，請繼續標注：）</div>
+      ) : decision ? (
+        <div className="working-area-container overflow-scroll validation-working-area">
+          <div className="working-article-title body-padding">
+            {decision.taskId.title}
+          </div>
+          <div
+            className="working-article-content body-padding"
+            onMouseUp={mouseUpHandler}
+          >
+            {decision.taskId.content}
+          </div>
+          <div className="justify-start mb-30 body-padding">
+            <div className="nowrap mr-10">問題：</div>
+            {decision.answerId.question}
+          </div>
+          <div className="justify-start mb-30 body-padding">
+            <FormControl component="fieldset">
+              <div class="radio-title">請選出下列較符合的答案：</div>
+              <RadioGroup
+                name="decision"
+                value={decisionResult}
+                onChange={handleDecisionChange}
+              >
+                <FormControlLabel
+                  value="answer"
+                  control={<Radio />}
+                  label={decision.answerId.answer}
+                />
+                <FormControlLabel
+                  value="validation"
+                  control={<Radio />}
+                  label={decision.validationId.validationAnswer}
+                />
+                <FormControlLabel
+                  value="NOTA"
+                  control={<Radio />}
+                  label="以上皆非"
+                />
+              </RadioGroup>
+            </FormControl>
+          </div>
+          <div className="justify-center">
+            <div onClick={() => saveDecision()}>
+              <div className="function-button">完成</div>
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="working-area-container overflow-scroll validation-working-area">
+          <div className="working-article-title body-padding">
+            {validation.title}
+          </div>
+          <div
+            className="working-article-content body-padding"
+            onMouseUp={mouseUpHandler}
+          >
+            {validation ? validation.taskId.content : ''}
+          </div>
+          <div className="justify-start mb-30 body-padding">
+            <div className="nowrap mr-10">問題：</div>
+            {validation.question}
+          </div>
+          <div className="justify-start body-padding">
+            <div className="nowrap mr-10">答案：</div>
+            <textarea
+              className="working-textarea"
+              value={validationAnswer}
+              onChange={() => {
+                return;
+              }}
+              placeholder="請透過滑鼠反白方式選擇文章中的答案"
+            />
+          </div>
+          <div className="justify-center">
+            <div onClick={() => saveValidation()}>
+              <div className="function-button">完成</div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
